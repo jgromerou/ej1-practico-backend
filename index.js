a9ba2f013318dec42f8288452fd7d1ff86efadf5
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-06-29 09:09:19 -0300

    se realizó la configuración inicial del proyecto backend

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import morgan from 'morgan';
+import path from 'path';
+
+dotenv.config();
+
+const app = express();
+
+app.set('PORT', process.env.PORT || 4000);
+
+app.listen(app.get('PORT'), () => {
+  console.log('Estoy en el puerto ' + app.get('PORT'));
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+//rutas
+app.get('/prueba', (req, res) => {
+  res.send('Esta es una prueba de mi ruta GET de prueba');
+});
